8765f906281d73fb1d7be4d54826f47d2352e16d
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-03-13 20:27:41 +0200

    Fill player Class

--- lib/player.rb
+++ lib/player.rb
@@ -1,4 +1,66 @@
 # frozen_string_literal: true
 
+# get input and store statistics
 class Player
+  attr_accessor :name, :score, :games
+
+  def initialize(name = 'Player')
+    self.name = name
+    # score and games aren't used for now, but I'll include them for future..
+    self.score = 0.0 # W/L
+    self.games = [] # all played games(or their represantation)
+  end
+
+  def make_guess
+    puts "Numbers represent colors: #{'1'.red}, #{'2'.blue}, #{'3'.yellow}, #{'4'.green}, #{'5'.magenta}, #{'6'.cyan}"
+    puts 'Enter your guess(4 digits):'
+    code = gets.strip
+
+    # ensure code is valid
+    until valid_code?(code)
+      puts "\nInvalid code! Try again"
+      code = gets.strip
+    end
+
+    # convert code to spheres and return it
+    convert_code(code)
+  end
+
+  private
+
+  def convert_code(number)
+    # get all possible colors
+    colors = Board.colors
+
+    # split number to array
+    numbers = number.split('')
+
+    # declare code array
+    code = []
+
+    # convert
+    numbers.each do |n|
+      code << '󰽢'.colorize(colors[n.to_i - 1])
+    end
+
+    code.join(' ')
+  end
+
+  def valid_code?(code)
+    # check size and type
+    return false unless code.is_a?(String) &&
+                        code.size == code.to_i.to_s.size &&
+                        code.size == 4
+
+    # convert to array of numbers
+    nums = code.split('').map(&:to_i)
+
+    # check if num is in colors scope
+    nums.each do |num|
+      return false if num.to_i > 6 || num.to_i < 1
+    end
+
+    # all checks passed
+    true
+  end
 end
