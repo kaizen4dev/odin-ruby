8f60b2213023dcc253d5cc2ddeaf693bbd65e86e
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-03-24 16:30:31 +0200

    Finish part 1 of assignment(clean phonenuber) Formated numbers stored in phonebook.txt

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -8,6 +8,17 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[...5]
 end
 
+def clean_phonenumber(number)
+  number = number.scan(/\d/)
+  size = number.size
+
+  number.shift if size == 11 && number[0] == 1
+
+  return 'invalid' unless size == 10
+
+  number.join('')
+end
+
 def legislators_by_zipcode(zipcode)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = File.read('key.txt').strip
@@ -33,6 +44,14 @@ def create_letter_file(id, letter)
   end
 end
 
+def add_to_phonebook(name, number)
+  file = File.open('phonebook.txt', 'a+')
+  entry = "#{name}: #{number}"
+
+  file.puts entry unless file.read.include?(entry)
+  file.close
+end
+
 puts 'Event Manager Initialized!'
 puts 'Processing...'
 
@@ -43,12 +62,15 @@ template = ERB.new File.read('letter-template.erb')
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
+  number = clean_phonenumber(row[:homephone])
   zipcode = clean_zipcode(row[:zipcode])
 
   legislators = legislators_by_zipcode(zipcode)
 
   letter = template.result(binding)
   create_letter_file(id, letter)
+
+  add_to_phonebook(name, number)
 end
 
 puts 'Done!'
