f583dfe30222def43ff932ad3b82c68e4bb17207
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-03-25 15:52:20 +0200

    Start building game class
    - attr_accessor for word, guesses, tries
    - initialize method
    - random_word method

---
+++ lib/game.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# logic of the game
+class Game
+  private
+
+  attr_accessor %i[word guesses tries]
+
+  def initialize
+    self.word = random_word
+    self.guesses = Array.new(word.size, '_')
+    self.tries = word.size / 2
+  end
+
+  def random_word
+    File.readlines('dict.txt').sample.strip
+  end
+end
