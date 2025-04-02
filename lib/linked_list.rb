965965f4e67d5a8900b89c5f5741823d0078cf03
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-04-02 21:04:00 +0300

    Create #append and #perpend

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -10,6 +10,21 @@ class LinkedList
     self.size = 0
   end
 
+  def prepend(value)
+    self.head_node = Node.new(value, head_node)
+    self.size += 1
+
+    self.tail_node = head_node if tail_node.nil?
+  end
+
+  def append(value)
+    return prepend(value) if head_node.nil?
+
+    new_node = Node.new(value)
+    tail_node.next = new_node
+    self.tail_node = new_node
+  end
+
   private
 
   attr_writer :head_node, :tail_node, :size
