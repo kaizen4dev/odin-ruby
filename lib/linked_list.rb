1d1d901b75033e1b0850f7744d8a1420c7396468
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-04-02 20:40:25 +0300

    Add attr writer and reader

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -4,5 +4,9 @@ require_relative 'node'
 
 # list of nodes, where each node holds certain value and points at next node
 class LinkedList
+  attr_reader :head_node, :tail_node, :size
 
+  private
+
+  attr_writer :head_node, :tail_node, :size
 end
