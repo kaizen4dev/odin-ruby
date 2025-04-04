69fdc9e18bb5e62e200f7d6bed8adee6604b039b
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-04-04 21:59:34 +0300

    Add useless main

--- main.rb
+++ main.rb
@@ -1,14 +1,36 @@
 # frozen_string_literal:true
 
-require_relative 'lib/linked_list'
+require_relative 'lib/hash_map'
 
-list = LinkedList.new
+test = HashMap.new
 
-list.append('dog')
-list.append('cat')
-list.append('parrot')
-list.append('hamster')
-list.append('snake')
-list.append('turtle')
+test.set('apple', 'red')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown')
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+test.set('grape', 'purple')
+test.set('hat', 'black')
+test.set('ice cream', 'white')
+test.set('jacket', 'blue')
+test.set('kite', 'pink')
+test.set('lion', 'golden')
 
-puts list
+p test.get('lion')
+
+test.remove('lion')
+
+p test.get('lion')
+
+p test.length
+
+p test.keys
+p test.values
+p test.entries
+
+1000.times do |i|
+  test.set(i.to_s, rand(2000))
+end
+
+p test.array.map(&:size)
