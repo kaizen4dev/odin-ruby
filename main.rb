150f1d1906e0cee47dc1dcbbe341c892f9b975db
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-04-21 14:05:16 +0300

    Add Move class to track moves

--- main.rb
+++ main.rb
@@ -1,9 +1,19 @@
 # frozen_string_literal: true
 
-def knight_moves(start_pos, _end_pos); end
+def knight_moves(start_pos, end_pos); end
 
 def possible_moves
   directions = [[-1, 2], [-2, 1], [1, -2], [2, -1], [-1, -2], [-2, -1], [1, 2], [2, 1]]
   moves = directions.map { |move| [move[0] + start_pos[0], move[1] + start_pos[1]] }
   moves.select { |move| move[0].between?(0, 7) && move[1].between?(0, 7) }
+end
+
+# represents current and previous squares
+class Move
+  attr_accessor :square, :from
+
+  def initialize(square, from = nil)
+    self.square = square
+    self.from = from
+  end
 end
