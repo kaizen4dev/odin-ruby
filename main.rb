93ed169811d793ee96a129e4afd763de73435099
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-04-21 14:13:36 +0300

    fix: #possible_moves should take an argument

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@
 
 def knight_moves(start_pos, end_pos); end
 
-def possible_moves
+def possible_moves(start_pos)
   directions = [[-1, 2], [-2, 1], [1, -2], [2, -1], [-1, -2], [-2, -1], [1, 2], [2, 1]]
   moves = directions.map { |move| [move[0] + start_pos[0], move[1] + start_pos[1]] }
   moves.select { |move| move[0].between?(0, 7) && move[1].between?(0, 7) }
