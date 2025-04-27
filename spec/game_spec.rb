2046db7809f3b363abd7f8b97259d18758f3b3f6
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-04-27 22:50:43 +0300

    Add descriptions for future tests/functionality

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,4 +3,19 @@
 require './lib/game'
 
 describe Game do
+  describe '#play' do
+    it 'shows tutorial(optional)'
+
+    it 'can exit game'
+
+    it 'can insert ball to the board'
+
+    it 'both players can make move'
+
+    it 'ends game after 4 balls connected'
+
+    it 'ends game in a draw after no space left'
+
+    it 'prompts for new game'
+  end
 end
