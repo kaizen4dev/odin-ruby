9882391d46f204268542f2dd0d9bd5a0de4365d5
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-04-27 22:10:03 +0300

    refactor: make board(attr_reader) private and #show public
    reasons:
    - board is a nested array and (will be) used only for implementing of another methods
    - #show prints formated board, which is more suited for user interaction

--- lib/game.rb
+++ lib/game.rb
@@ -6,17 +6,15 @@ class Game
   BALL1 = '⚽'
   BALL2 = '⚾'
 
-  attr_reader :board
+  def show
+    puts board.map(&:join) << '1|2|3|4|5|6|7'
+  end
 
   private
 
-  attr_writer :board
+  attr_accessor :board
 
   def initialize
     self.board = Array.new(6) { Array.new(7, EMPTY_SPACE) }
-  end
-
-  def show
-    puts board.map(&:join) << '1|2|3|4|5|6|7'
   end
 end
