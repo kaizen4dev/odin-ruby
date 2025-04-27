e7be797f77e5b7f1d2046905b485f5772df1314e
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-04-27 22:24:39 +0300

    Remove deprecated tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,21 +3,4 @@
 require './lib/game'
 
 describe Game do
-  describe '#board' do
-    it 'created upon initialization' do
-      expect(Game.new.board).to eql(Array.new(6, Array.new(7, '⚫')))
-    end
-  end
-
-  describe '#ball1' do
-    it 'is a soccer ball' do
-      expect(Game.new.ball1).to eql('⚽')
-    end
-  end
-
-  describe '#ball2' do
-    it 'is a baseball' do
-      expect(Game.new.ball2).to eql('⚾')
-    end
-  end
 end
