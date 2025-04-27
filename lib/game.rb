f045822caf0c93451b244072c148f9c8e7dcada7
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-04-27 19:14:16 +0300

    Create board upon Game initialization

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
+# connect four game class
 class Game
+  attr_reader :board
+
+  private
+
+  attr_writer :board
+
+  def initialize
+    self.board = Array.new(6) { Array.new(7, '⚫') }
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,4 +3,9 @@
 require './lib/game'
 
 describe Game do
+  describe '#board' do
+    it 'created upon initialization' do
+      expect(Game.new.board).to eql(Array.new(6, Array.new(7, '⚫')))
+    end
+  end
 end
